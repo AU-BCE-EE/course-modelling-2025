0fc0f95585359946af434d2eb84f3c10f303367a
Frederik Rask Dalby <au277187@uni.au.dk>
2025-11-11 16:37:14 +0100

    upload mass_cons code

--- classes/08_18_sept_1D_method_of_lines/demos/diff1D_model_mass_cons.py
+++ classes/08_18_sept_1D_method_of_lines/demos/diff1D_model_mass_cons.py
@@ -1,10 +1,3 @@
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 25 12:38:54 2025
-
-@author: au277187
-"""
 import numpy as np 
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
@@ -26,10 +19,7 @@ def rates(t, c, dx, x_grid):
     
     dcdt = np.zeros(len(x_grid))
     N = len(x_grid)-1
-    
-    dcdt[0] = 0
-    dcdt[-1] = 0
-    
+       
     for i in range(1, N):
         dcdt[i] = D * (c[i-1] - 2 * c[i] + c[i+1])/dx**2
     
@@ -42,33 +32,46 @@ tmax = 1000 # s
 sol = solve_ivp(rates, t_span = [0, tmax], y0 = c0, method = 'LSODA',
                 t_eval = np.linspace(0, tmax, 1000),
                 args = (dx, x_grid))
-      
+  
 for i in range(0, len(x_grid)-1, int((len(x_grid)-1)/5)):
     plt.plot(sol.t, sol.y[i], label = f'{i*dx} m')
 plt.legend(loc = 1)
 plt.ylabel('concentration, mg/m3')
 plt.xlabel('time, s')
 
-# flux = J = -D * dc/dx: J = -D * (c[1] - c[0])/dx
-
+# How much O2 has diffused in and out of the model domain?
+# flux into model domaim: J = -D * dc/dx
+# J = -D * (c[i+1] - c[i])/dx: J = -D * (c[1] - c[0])/dx
 c_0 = sol.y[0, :]
 c_1 = sol.y[1, :]
 
-flux_in = -D * (c_1 - c_0) /dx
+flux_in = -D * (c_1-c_0)/dx # unit is mg/m2/s
 
+# flux out of domain: 
+# J = -D * (c[i+1] - c[i])/dx
 cN = sol.y[-1, :]
 cN2 = sol.y[-2, :]
-flux_out  = -D * (cN - cN2)/dx
+
+flux_out = -D * (cN - cN2)/dx
+
+time = sol.t
+plt.plot(time, flux_in, label = "flux_in")
+plt.plot(time, flux_out, label = "flux_out")
+plt.legend(loc = 1)
+plt.xlabel('time, s')
+plt.ylabel('mg/m2/s')
 
 flux_in_cum = np.trapz(flux_in, sol.t)
 flux_out_cum = np.trapz(flux_out, sol.t)
 
-net_flux_in = flux_in_cum - flux_out_cum
-print(net_flux_in)
+net_flux_cum = flux_in_cum - flux_out_cum
+print(net_flux_cum)
+total_O2_end = np.trapz(sol.y[:, -1], x_grid)
+print(total_O2_end)
 
-last_O2 = np.trapz(sol.y[:, -1], x_grid)
-print(last_O2)
-last_O2 - c0[0] * dx/2
+print(c0)
+initial_C = c0[0] * dx/2
+print(total_O2_end - initial_C)
 
 
 def rates2(t, c, dx, x_grid, c_left, c_right):
@@ -93,22 +96,25 @@ sol2 = solve_ivp(rates2, t_span = [0, tmax], y0 = c0, method = 'LSODA',
                 t_eval = np.linspace(0, tmax, 1000),
                 args = (dx, x_grid, c_left, c_right))
 
-# flux = J = -D * dc/dx: J = -D * (c[i+1] - c[i-1])/(2*dx):
-    
-c_1 = sol2.y[1,:]    
-flux_in = -D * (c_1 - c_left)/(2*dx)
+# flux = J = -D * (c[i+1] - c[i-1])/(2*dx)
+c_1 = sol2.y[1, :]
+flux_in = -D * (c_1 - c_left)/(2*dx) # unit is mg/m2/s
 
-cN2 = sol2.y[-2,:]
-
+# flux out of domain: 
+# J = -D * (c[i+1] - c[i])/dx
+cN2 = sol2.y[-2, :]
 flux_out = -D * (c_right - cN2)/(2*dx)
 
 flux_in_cum = np.trapz(flux_in, sol2.t)
 flux_out_cum = np.trapz(flux_out, sol2.t)
 
-net_flux_in = flux_in_cum - flux_out_cum
-print(net_flux_in)
+net_flux_cum = flux_in_cum - flux_out_cum
+total_O2_end = np.trapz(sol2.y[:, -1], x_grid)
 
-last_O2 = np.trapz(sol2.y[:,-1], x_grid)
-last_O2
+print(net_flux_cum)
+print(total_O2_end)
 
-c0
+plt.plot(flux_out)
+
+initial_C = c0[0] * dx/2
+print(total_O2_end - initial_C)
